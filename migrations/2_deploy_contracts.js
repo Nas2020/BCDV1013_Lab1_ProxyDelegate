7835f3bdb891dbe52832ad93db27f8c56bc3edbc
Nas2020 <naseer8518@gmail.com>
2020-07-11 11:01:21 -0400

    Lab1 fix test cases

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const ProxyDelegate = artifacts.require('ProxyDelegate');
+const SomeLibrary = artifacts.require('SomeLibrary');
+const ProxyCall = artifacts.require('ProxyCall');
+
+module.exports = function(deployer) {
+    deployer.deploy(SomeLibrary)
+    .then(() => deployer.deploy(ProxyDelegate, SomeLibrary.address))
+    .then(() => deployer.deploy(ProxyCall, SomeLibrary.address));
+}
